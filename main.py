8d57d9415a4fe2131de67989ad3b205cbb6cab66
gpib <rafal.koslowski@gmail.com>
2024-01-23 17:02:30 +0100

    trying to commit

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+#comment
